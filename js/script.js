1d18ea89989c3f9fee0f318298e79a0eaaca9cb8
LaraOlivares <laraolivaresdimatteo@gmail.com>
2024-08-21 01:13:01 -0300

    Completed

--- js/script.js
+++ js/script.js
@@ -19,3 +19,15 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+fetch(DATA_URL)
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }  //Si la promesa es devuelta como error o no es devuelta, salta el error
+    return response.json(); // Convierte la respuesta a JSON
+})
+.then(data => { 
+  const students = data.students;
+  showData(students)
+}) //Ejecuta la función showData con los datos del array 
